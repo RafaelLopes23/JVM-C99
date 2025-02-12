5db6183d3bd9910b824ffbd2518861f73b6ae631
Dickmann25 <106775626+Dickmann25@users.noreply.github.com>
2025-02-11 21:04:59 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include "jvm.h"
+#include "../leitor-exibidor/read_count_func.h"
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
@@ -7,10 +8,35 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    JVM jvm;
-    jvm_init(&jvm);
-    jvm_load_class(&jvm, argv[1]);
-    jvm_execute(&jvm);
+    if (strcmp(argv[2], "--leitor") == 0) {
+        read_magic_number(argv[1]); // Lê e exibe o magic number do arquivo .class
+        read_minor_major(argv[1]); // Lê e exibe o minor major number
+        ConstantPoolResult pos = read_constant_pool(argv[1]);
+        printf("\n--------------------------------------------------------------------------------------------------------------------------------------\n");
+        read_access_flags(argv[1], &pos, 0);
+        read_this_class(argv[1], &pos);
+        read_super_class(argv[1], &pos);
+        printf("\n--------------------------------------------------------------------------------------------------------------------------------------\n");
+        uint16_t i_count = read_interfaces_count(argv[1], &pos);
+        display_interfaces(argv[1], i_count, &pos);
+        printf("\n--------------------------------------------------------------------------------------------------------------------------------------\n");
+        uint16_t f_count = read_fields_count(argv[1], &pos);
+        display_fields(argv[1], f_count, &pos);
+        printf("\n--------------------------------------------------------------------------------------------------------------------------------------\n");
+        uint16_t m_count = read_method_count(argv[1], &pos);
+        display_method(argv[1], m_count, &pos);
+        printf("\n--------------------------------------------------------------------------------------------------------------------------------------\n");
+        uint16_t a_count = read_atribute_count(argv[1], &pos);
+        display_atribute(argv[1], a_count, &pos);
+        return EXIT_SUCCESS;
+    }
 
-    return 0;
+    if (strcmp(argv[2], "--jvm") == 0) {
+        JVM jvm;
+        jvm_init(&jvm);
+        jvm_load_class(&jvm, argv[1]);
+        jvm_execute(&jvm);
+
+        return 0;
+    }
 }
